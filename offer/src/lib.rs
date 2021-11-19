b90f00c53ea2cbc98c61d237457f9c687d4ee349
Akagi201 <akagi201@gmail.com>
2021-11-20 00:49:32 +0800

    feat(leetcode): add offer04, offer53_i, offer53_ii

--- offer/src/lib.rs
+++ offer/src/lib.rs
@@ -6,3 +6,6 @@ mod offer06;
 mod offer24;
 mod offer05;
 mod offer58;
+mod offer53_i;
+mod offer53_ii;
+mod offer04;
---
+++ offer/src/offer53_i.rs
@@ -0,0 +1,18 @@
+// https://leetcode-cn.com/problems/zai-pai-xu-shu-zu-zhong-cha-zhao-shu-zi-lcof/
+
+struct Solution;
+
+#[allow(unused)]
+impl Solution {
+  pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+    nums.iter().filter(|&&x| x == target).count() as i32
+  }
+}
+
+#[test]
+fn tests() {
+  assert_eq!(Solution::search(vec![], 0), 0);
+  assert_eq!(Solution::search(vec![1], 1), 1);
+  assert_eq!(Solution::search(vec![5, 7, 7, 8, 8, 10], 8), 2);
+  assert_eq!(Solution::search(vec![5, 7, 7, 8, 8, 10], 6), 0);
+}
